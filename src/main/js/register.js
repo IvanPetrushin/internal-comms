3a68b2437ed9ebe966b0d0ba08918a2c4eaea2e0
Ivan <ivan24891@mail.ru>
2023-04-11 23:05:19 +0300

    register form modified

--- src/main/js/register.js
+++ src/main/js/register.js
@@ -1,8 +1,9 @@
 const inputsPass = document.querySelectorAll('input[type=password]');
 const iconsPass = document.querySelectorAll('span[type=img]');
-const buttons = document.querySelectorAll('input[type=submit]');
-const form = document.querySelector('form');
-
+const inputsSignIn = [].slice.call(document.querySelectorAll('input[data-rule="sign-in"]'));
+const inputsSignUp = [].slice.call(document.querySelectorAll('input[data-rule="sign-up"]'));
+const signInButton = document.getElementById('sign-in');
+const signUpButton = document.getElementById('sign-up');
 iconsPass.forEach(icon => {
     icon.addEventListener('click', () => {
         inputsPass.forEach(input => {
@@ -14,16 +15,38 @@ iconsPass.forEach(icon => {
         });
     });
 });
-
-form.addEventListener('change', changeFormHandler)
-
-function changeFormHandler() {
-    console.log(form.checkValidity());
-    if (form.checkValidity()) {
-        buttons.forEach(button => {
-            button.removeAttribute('disabled');
-        });
-    }
+inputsSignIn.forEach(function(el){
+    el.addEventListener('input', checkInputsSignIn, false);
+});
+function checkInputsSignIn(){
+    let empty = inputsSignIn.filter(function(el){
+        return el.value.trim() === '';
+    }).length;
+    signInButton.disabled = empty !== 0;
 }
+
+inputsSignUp.forEach(function(el){
+    el.addEventListener('input', checkInputsSignUp, false);
+});
+function checkInputsSignUp(){
+    let empty = inputsSignUp.filter(function(el){
+        return el.value.trim() === '';
+    }).length;
+    signUpButton.disabled = empty !== 0;
+}
+
+// signUpButton.addEventListener('click', () => {
+//     if (document.getElementById('password-sign-up').value === document.getElementById('password-sign-up-again').value) {
+//         return true
+//     } else {
+//         alert("Не совпадают пароли")
+//         signUpButton.disabled = true
+//         return false
+//     }
+// })
+
+checkInputsSignUp()
+checkInputsSignIn()
+
 
 
