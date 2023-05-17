4b815b1088190a27a0be3e1502776c7adae79d2f
Ivan <ivan24891@mail.ru>
2023-05-17 16:03:19 +0300

    что-то добавил, что-то убавил

--- src/main/resources/js/registerRequests.js
+++ src/main/resources/js/registerRequests.js
@@ -4,29 +4,24 @@ const url = 'http://localhost:8080/users/login'
 //let response = await fetch(url)
 
 if (signInButton) {
-    async function sendUserInfo() {
        // response = await fetch(url)
-        signInButton.addEventListener('click', /*для отлади заменить 'submit' -> 'click' */ async () => {
-            let response = await fetch(url, { // Отправляем JSON
-                method: 'POST',
-                body: dataSignIn,
-                headers: {'Content-type': 'application.json'}
-            })
-            let incomeData = await response.json() // Читаем ответ
-            console.log(incomeData)
+    signInButton.addEventListener('click', /*для отлади заменить 'submit' -> 'click' */ async () => {
+        let response = await fetch(url, { // Отправляем JSON
+            method: 'POST',
+            body: dataSignIn,
+            headers: {'Content-type': 'application.json'}
         })
-    }
+        let incomeData = await response.json() // Читаем ответ
+        console.log(incomeData)
+    })
 
-    async function getUserInfo() {
+    let response = await fetch(url);
+    signInButton.addEventListener('click', async () => {
         let response = await fetch(url);
-        signInButton.addEventListener('click', async () => {
-            let response = await fetch(url);
-            let info = JSON.parse(JSON.stringify(await response.json()));
-            if (info == null) {
-                alert("Ошибка")
-                signInButton.formAction = "";
-            }
-        } )
-
-    }
+        let info = JSON.parse(JSON.stringify(await response.json()));
+        if (info == null) {
+            alert("Ошибка")
+            signInButton.formAction = "";
+        }
+    } )
 }
