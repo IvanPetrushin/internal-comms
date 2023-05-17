5601f87e8d73919fdc7a9542e96fdc2adfb846b8
Ivan <ivan24891@mail.ru>
2023-05-17 13:53:26 +0300

    что-то добавил, что-то убавил

--- src/main/java/internalcomms/Controllers/MainController.java
+++ src/main/java/internalcomms/Controllers/MainController.java
@@ -1,19 +1,19 @@
-package internalcomms.Controllers;
-
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class MainController {
-
-    @GetMapping("/register")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "register";
-    }
-
-
-}
+//package internalcomms.Controllers;
+//
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//public class MainController {
+//
+//    @GetMapping("/register")
+//    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "register";
+//    }
+//
+//
+//}
--- src/main/resources/js/registerRequests.js
+++ src/main/resources/js/registerRequests.js
@@ -3,9 +3,10 @@ const url = 'http://localhost:8080/users/'
 
 
 if (signInButton) {
+   // response = await fetch(`${u}`)
     signInButton.addEventListener('click', /*для отлади заменить 'submit' -> 'click' */ async () => {
         let response = await fetch(url, { // Отправляем JSON
-            method: 'POST',
+            method: 'GET',
             body: dataSignIn,
             headers: {'Content-type': 'application.json'}
         })
